b1fcb22a81de4f62dd5abe89c880c35da558203e
arunk28 <arunkumar280491@gmail.com>
2021-05-26 19:44:15 +0530

    commets added

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-
 const {app,BrowserWindow}  = electron;
 
 let mainWindow ;
 
-
+//Listen to tjhe app at ready
 app.on('ready',function(){
 
+//create new window
 mainWindow = new BrowserWindow({});
 
+//load the html to the window
 mainWindow.loadURL(url.format({
     pathname: path.join(__dirname,'welcome.html'),
     protocol : 'file:',
